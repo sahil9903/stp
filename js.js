477771eb14bd9637531e63d743658cb220490085
sahil9903 <104521385+sahil9903@users.noreply.github.com>
2023-10-25 20:50:54 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+const body = document.querySelector("body")
+
 const loginbutton = document.querySelector(".loginbtn")
 const loginclose = document.querySelector(".loginclose")
 const logincontainer = document.querySelector(".logincontainer")
@@ -6,7 +8,25 @@ const togglelogin = ()=>{
 }
 loginbutton.addEventListener("click",()=>{
     togglelogin()
+    body.style.overflowY = 'hidden'
 })
 loginclose.addEventListener("click",()=>{
     togglelogin()
+    body.style.overflowY = 'scroll'
+})
+
+const signbutton = document.querySelector(".signbtn")
+const signclose = document.querySelector(".signclose")
+const signcontainer = document.querySelector(".signcontainer")
+const togglesign = ()=>{
+    signcontainer.classList.toggle("active2")
+}
+signbutton.addEventListener("click",()=>{
+    togglesign()
+    body.style.overflowY = 'hidden'
+})
+signclose.addEventListener("click",()=>{
+    togglesign()
+    body.style.overflowY = 'scroll'
+    
 })
